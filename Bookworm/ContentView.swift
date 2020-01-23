e523d4a22099c0ae9d12aa6bfebc0766851301e8
s4rv4d <sarvadshetty@gmail.com>
2020-01-23 11:22:58 +0530

    intro to @Binding

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,13 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var remeberMe = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            PushButton(title: "Remember me?", isOn: $remeberMe)
+            Text(remeberMe ? "On" : "Off")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+//custom button
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            self.isOn.toggle()
+        }
+        
+    .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+    .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
     }
 }
